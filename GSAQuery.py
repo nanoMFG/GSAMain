73b93a41b57c0a3dcb2e1a9be58069e12b3e94d0
Joshua Schiller <jaschil2@illinois.edu>
2018-10-15 13:39:32 -0500

    Edited database and testing script some more

--- GSAQuery.py
+++ GSAQuery.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import scipy as sc
 import sys
+from PyQt5 import QtGui, QtCore
 from GSAImage import GSAImage
 
 class GSAQuery:
@@ -11,105 +12,58 @@ class GSAQuery:
 		self.layout = QtGui.QGridLayout()
 		self.layout.setAlignment(QtCore.Qt.AlignTop)
 
-		self.searchOptions = QtGui.QStackedWidget()
-		self.searchOptions.setFixedWidth(300)
-		self.criteria = {
-			'Furnace Criteria': FurnaceCriterion(),
-			'Flow Criteria': FlowCriterion()
-		}
-		self.criteriaBox = QtGui.QComboBox()
-		for c in self.criteria.keys():
-			self.criteriaBox.addItem(c)
-			self.searchOptions.addWidget(self.criteria[c].widget())
+		self.layout.addWidget(FilterWidget(),0,0)
 
-		self.filterList = QtGui.QListWidget()
-		self.resultsList = QtGui.QListWidget() # Use Table Widget
-		self.addFilterBtn = QtGui.QPushButton('Add Filter')
-		self.searchBtn = QtGui.QPushButton('Search')
-		self.imagePreview =  pg.LayoutWidget()
-		with open('temp_1538149879.json','r') as f:
-			d = json.load(f)
-		self.imagePreview.addWidget(ImageAnalyzer.viewOnlyWidget(d),0,0)
-		self.imagePreview.setFixedHeight(300)
-
-		self.addFilterBtn.clicked.connect(lambda: self.addFilter())
-		self.criteriaBox.currentIndexChanged.connect(self.searchOptions.setCurrentIndex)
-
-		self.layout.addWidget(self.criteriaBox,0,0)
-		self.layout.addWidget(self.searchOptions,1,0)
-		self.layout.addWidget(self.addFilterBtn,2,0)
-		self.layout.addWidget(self.filterList,3,0)
-		self.layout.addWidget(self.searchBtn,4,0)
-		self.layout.addWidget(self.imagePreview,0,1,2,2)
-		self.layout.addWidget(self.resultsList,2,1,3,2)
 		self.main.setLayout(self.layout)
-
-	def addFilter(self):
-		key = self.criteriaBox.currentText()
-		self.filterList.addItem("%d %s"%(self.filterList.count(),key))
-		self.filters.append(self.criteria[key].getValues())
-		self.criteria[key].clear()
-
-		print(self.filters)
 
 	def widget(self):
 		return self.main
 
-class Criterion:
-	def __init__(self):
-		self.main = QtGui.QWidget()
-		self.layout = QtGui.QGridLayout()
-		self.layout.setAlignment(QtCore.Qt.AlignTop)
-		self.fields = {}
-		self.values = {}
+	def run(self):
+		self.main.show()
 
-	def widget(self):
-		return self.main
+class FilterWidget(QtGui.QWidget):
+	def __init__(self,parent=None):
+		super(FilterWidget,self).__init__(parent=parent)
+		layout = QtGui.QGridLayout(self)
 
-	def clear(self):
-		pass
+		self.criteria = [
+			'furnace_temperature',
+			'furnace_pressure',
+			'catalyst'
+		]
 
-class FurnaceCriterion(Criterion):
-	def __init__(self):
-		super(FurnaceCriterion,self).__init__()
-		self.criteria = ['Furnace Criterion %d'%i for i in range(5)]
-		
-		for i,c in enumerate(self.criteria):
-			self.fields[c] = QtGui.QLineEdit()
-			self.values[c] = None
-			self.fields[c].setPlaceholderText('Example Text %d'%i)
-			self.layout.addWidget(QtGui.QLabel(c),i,0)
-			self.layout.addWidget(self.fields[c],i,1)
+		self.widgets = {}
 
-		self.main.setLayout(self.layout)
+		for c,cr in enumerate(self.criteria):
+			row = FilterRow()
+			row.label.setText(cr)
+			layout.addWidget(row,c,0)
+			self.widgets[cr] = row
 
-	def getValues(self):
-		for field in self.fields.keys():
-			self.values[field] = self.fields[field].text()
-		return self.values
+	def getFilters(self):
+		filters = {}
+		for widget in self.widgets:
+			filters[widget.label.text()] = 
 
-class FlowCriterion(Criterion):
-	def __init__(self):
-		super(FlowCriterion,self).__init__()
-		self.criteria = ['Flow Criterion %d'%i for i in range(5)]
-		
-		for i,c in enumerate(self.criteria):
-			self.fields[c] = QtGui.QLineEdit()
-			self.values[c] = None
-			self.fields[c].setPlaceholderText('Example Text %d'%i)
-			self.layout.addWidget(QtGui.QLabel(c),i,0)
-			self.layout.addWidget(self.fields[c],i,1)
+class FilterRow(QtGui.QWidget):
+	def __init__(self,parent=None):
+		super(FilterRow,self).__init__(parent=parent)
+		layout = QtGui.QGridLayout(self)
+		self.label = QtGui.QLabel()
+		self.label.setFixedWidth(150)
+		self.comparator = QtGui.QComboBox()
+		self.comparator.addItems(['=','!=','<','<=','>','>='])
+		self.input = QtGui.QLineEdit()
+		self.input.setFixedWidth(100)
 
-		self.main.setLayout(self.layout)
-
-	def getValues(self):
-		for field in self.fields.keys():
-			self.values[field] = self.fields[field].text()
-		return self.values
+		layout.addWidget(self.label,0,0)
+		layout.addWidget(self.comparator,0,1)
+		layout.addWidget(self.input,0,2)
 
 
-
-
-
-
-
+if __name__ == '__main__':
+	app = QtGui.QApplication([])
+	query = GSAQuery()
+	query.run()
+	sys.exit(app.exec_())
