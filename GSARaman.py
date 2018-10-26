909be13bcd85a35307a74ed6a3ad99ff7ee0c768
Kris Miller <krismiller@wirelessprv-10-195-177-199.near.illinois.edu>
2018-10-26 10:55:57 -0500

    Added fitting

--- GSARaman.py
+++ GSARaman.py
@@ -1,13 +1,25 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
 import pyqtgraph as pg
 import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
 
+pg.setConfigOption('background','w')
+pg.setConfigOption('foreground','k')
 
 filelist=[]
+layer1=[{'a':3.00007920e-01,'w':3.73588869e+01,'b':1.58577373e+03},{'a':1.00000000e+00,'w':3.25172389e+01,'b':2.68203383e+03}]
+layer2=[{'a':1.04377489e+00,'w':3.34349819e+01,'b':1.59438802e+03},{'a':7.06298092e-01,'w':6.14683794e+01,'b':2.70286968e+03}]
+layer3=[{'a':1.04128278e+00,'w':2.63152833e+01,'b':1.60154940e+03},{'a':6.50155655e-01,'w':5.73486165e+01,'b':2.72324859e+03}]
+layer4=[{'a':1.01520762e+00,'w':2.79110458e+01,'b':1.61188139e+03},{'a':5.18657822e-01,'w':6.83826156e+01,'b':2.73099972e+03}]
+layer5=[{'a':9.67793017e-01,'w':2.80824430e+01,'b':1.62490732e+03},{'a':4.30042148e-01,'w':6.41600512e+01,'b':2.75285511e+03}]
+graphene=[{'a':9.98426340e-01,'w':2.83949973e+01,'b':1.63840546e+03},{'a':4.22730948e-01,'w':7.98338055e+01,'b':2.76274546e+03}]
+cdat={'mono':layer1,'bi':layer2,'tri':layer3,'quad':layer4,'pent':layer5,'graph':graphene}
 
 class GSARaman(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(GSARaman,self).__init__(parent=parent)
+        self.resize(500,500)
         self.spect_type=''
         self.data=[]
 
@@ -49,9 +61,9 @@ class GSARaman(QtWidgets.QWidget):
         self.checkFileType()
         if self.spect_type=='single':
             self.widget=SingleSpect()
-            x=self.data.iloc[:,0]
-            y=self.data.iloc[:,1]
-            self.widget.graphSpect(x,y)
+            x=np.array(self.data.iloc[:,0])
+            y=np.array(self.data.iloc[:,1])
+            self.widget.plotSpect(x,y)
         else:
             self.widget=MapFit()
 
@@ -60,9 +72,51 @@ class SingleSpect(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(SingleSpect,self).__init__(parent=parent)
 
-    def graphSpect(self,x,y):
+    def Single_Lorentz(self, x,a,w,b):
+        return a*(((w/2)**2)/(((x-b)**2)+((w/2)**2)))
+
+    def Lfit(self,x,a1,w1,b1,a2,w2,b2,a3,w3,b3):
+        LGfit=self.Single_Lorentz(x,a1,w1,b1)
+        LGpfit=self.Single_Lorentz(x,a2,w2,b2)
+        LDfit=self.Single_Lorentz(x,a3,w3,b3)
+        Lfit=LGfit+LGpfit+LDfit
+        return Lfit
+
+    def backgroundFit(self,x,y):
+        I_raw=y
+        W=x
+
+        polyx=np.array([W[0],W[int(len(W)/2)],W[len(W)-1]])
+        polyy=np.array([I_raw[0],I_raw[int(len(W)/2)],I_raw[len(W)-1]])        
+        bkgfit=np.polyfit(polyx,polyy,2)
+        bkgpoly=(bkgfit[0]*W**2)+(bkgfit[1]*W)+bkgfit[2]
+        I_raw=I_raw-bkgpoly
+    
+        m=(I_raw[len(W)-1]-I_raw[0])/(W[len(W)-1]-W[0])
+        b=I_raw[len(W)-1]-m*W[len(W)-1]
+        bkglin=m*W+b
+    
+        I_raw=I_raw-bkglin
+    
+        I=((I_raw-np.min(I_raw))/np.max(I_raw-np.min(I_raw)));
+        return I
+
+
+    def fitToPlot(self,x,y):
+        I=self.backgroundFit(x,y)
+        fit_params,fit_cov=curve_fit(self.Lfit,x,y,
+            bounds=([0.3*np.max(I),33,1400,0.3*np.max(I),32,2000,0,10,1300],[1.5*np.max(I),60,2000,1.5*np.max(I),60,3000,np.max(I),50,1400]))
+        
+        y_fit=self.Lfit(x,fit_params[0],fit_params[1],fit_params[2],fit_params[3],fit_params[4],fit_params[5],fit_params[6],fit_params[7],fit_params[8])
+        self.fit_plot=pg.plot(x,y_fit)
+
+    def plotSpect(self,x,y):
         self.spect_plot=pg.plot(x,y)
+        self.fitToPlot(x,y)
+
         raman.layout.addWidget(self.spect_plot,2,0)
+        raman.layout.addWidget(self.fit_plot,2,1)
+
 
 
 
