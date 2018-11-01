63d06e033fee17f40e05eb5f441040c593e799eb
Kevin Liu <kevindl2@illinois.edu>
2018-11-01 11:28:40 -0500

    Saving to local repo before pulling new DrawScale

--- GSAImage.py
+++ GSAImage.py
@@ -236,10 +236,10 @@ class GSAImage:
 
 	def selectMod(self,index):
 		if index >= 0:
-			# try:
-			self.modifications[index].update_view()
-			# except:
-				# pass
+			try:
+			    self.modifications[index].update_view()
+			except:
+				pass
 			self.wDetail.setCurrentIndex(index)
 		elif self.wModList.count() > 0:
 			self.wModList.setCurrentRow(self.wModList.count()-1)
@@ -356,68 +356,70 @@ class Modification:
 		return cls(mod_in,img_item,d['properties'])
 
 class InitialImage(Modification):
-	def name(self):
-		return 'Initial Image'
-	def to_dict(self):
-		d = super(InitialImage,self).to_dict()
-		d['img_out'] = self.img_out.tolist()
-		return d
+  def name(self):
+    return 'Initial Image'
 
-	@classmethod
-	def from_dict(cls,d,img_item):
-		obj = super(InitialImage,cls).from_dict(d,img_item)
-		obj.set_image(np.asarray(d['img_out']))
-		return obj
+  def to_dict(self):
+    d = super(InitialImage, self).to_dict()
+    d['img_out'] = self.img_out.tolist()
+    return d
+
+  @classmethod
+  def from_dict(cls, d, img_item):
+    obj = super(InitialImage, cls).from_dict(d, img_item)
+    obj.set_image(np.asarray(d['img_out']))
+    return obj
+
 
 class ColorMask(Modification):
-	def __init__(self,mod_in,img_item,properties={}):
-		super(ColorMask,self).__init__(mod_in,img_item,properties)
-		self.img_mask = None
-		self.img_hist = self.img_item.getHistogram()
+  def __init__(self, mod_in, img_item, properties={}):
+    super(ColorMask, self).__init__(mod_in, img_item, properties)
+    self.img_mask = None
+    self.img_hist = self.img_item.getHistogram()
 
-		self.wHistPlot = None
-		self.lrItem = None
+    self.wHistPlot = None
+    self.lrItem = None
 
-		self.wHistPlot = pg.PlotWidget()
-		self.wHistPlot.plot(*self.img_hist)
-		self.wHistPlot.setXRange(0,255)
-		self.wHistPlot.hideAxis('left')
-		
-		self.lrItem = pg.LinearRegionItem((0,255),bounds=(0,255))
-		self.lrItem.sigRegionChanged.connect(self.update_view)
-		self.lrItem.sigRegionChangeFinished.connect(self.update_view)
-		
-		self.wHistPlot.addItem(self.lrItem)
-		self.wHistPlot.setMouseEnabled(False,False)
-		self.wHistPlot.setMaximumHeight(100)
+    self.wHistPlot = pg.PlotWidget()
+    self.wHistPlot.plot(*self.img_hist)
+    self.wHistPlot.setXRange(0, 255)
+    self.wHistPlot.hideAxis('left')
 
-	def to_dict(self):
-		d = super(ColorMask,self).to_dict()
-		d['img_mask'] = self.img_mask.tolist()
-		d['LinearRegionItem'] = {'region':self.lrItem.getRegion()}
-		return d
+    self.lrItem = pg.LinearRegionItem((0, 255), bounds=(0, 255))
+    self.lrItem.sigRegionChanged.connect(self.update_view)
+    self.lrItem.sigRegionChangeFinished.connect(self.update_view)
 
-	@classmethod
-	def from_dict(cls,d,img_item):
-		obj = super(ColorMask,cls).from_dict(d,img_item)
-		obj.img_mask = np.asarray(d['img_mask'])
-		obj.lrItem.setRegion(d['LinearRegionItem']['region'])
-		obj.update_image()
-		obj.widget().hide()
-		return obj
-	
-	def widget(self):
-		return self.wHistPlot
+    self.wHistPlot.addItem(self.lrItem)
+    self.wHistPlot.setMouseEnabled(False, False)
+    self.wHistPlot.setMaximumHeight(100)
 
-	def update_image(self):
-		minVal, maxVal = self.lrItem.getRegion()
-		img = self.mod_in.image()
-		self.img_mask = np.zeros_like(img)
-		self.img_mask[np.logical_and(img>minVal,img<maxVal)] = 1
-		self.img_out = img*self.img_mask+(1-self.img_mask)*255
+  def to_dict(self):
+    d = super(ColorMask, self).to_dict()
+    d['img_mask'] = self.img_mask.tolist()
+    d['LinearRegionItem'] = {'region': self.lrItem.getRegion()}
+    return d
 
-	def name(self):
-		return 'Color Mask'    
+  @classmethod
+  def from_dict(cls, d, img_item):
+    obj = super(ColorMask, cls).from_dict(d, img_item)
+    obj.img_mask = np.asarray(d['img_mask'])
+    obj.lrItem.setRegion(d['LinearRegionItem']['region'])
+    obj.update_image()
+    obj.widget().hide()
+    return obj
+
+  def widget(self):
+    return self.wHistPlot
+
+  def update_image(self):
+    minVal, maxVal = self.lrItem.getRegion()
+    img = self.mod_in.image()
+    self.img_mask = np.zeros_like(img)
+    self.img_mask[np.logical_and(img > minVal, img < maxVal)] = 1
+    self.img_out = img * self.img_mask + (1 - self.img_mask) * 255
+
+  def name(self):
+    return 'Color Mask'
 
 class CannyEdgeDetection(Modification):
 	def __init__(self,mod_in,img_item,properties={}):
