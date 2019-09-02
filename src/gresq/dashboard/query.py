3bfdf23ad1d4665f5917d29d731c4156e4f68e17
Darren Adams <dadams@illinois.edu>
2019-09-02 09:10:39 -0400

    Cleaned up imports

--- src/gresq/dashboard/query.py
+++ src/gresq/dashboard/query.py
@@ -1,7 +1,4 @@
 from __future__ import division
-import os
-import sys
-#sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),'gsaimage','src'))
 import pandas as pd
 import numpy as np
 import sys, operator, os
