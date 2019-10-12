cc0650391c4f5c97a526d94ee704ab297eeb959f
Darren Adams <dadamsncsa@gmail.com>
2019-10-12 08:36:57 -0500

    mostly test updates

---
+++ test/database/__init__.py
@@ -0,0 +1,9 @@
+from gresq.config import Config
+from gresq.database import dal
+
+config_prefix = 'TEST_DATABASE'
+config_suffix = '_ADMIN'
+
+#print("HHHHHHEEEEEYYYYY")
+conf = Config(prefix=config_prefix, suffix=config_suffix, debug=True, try_secrets=False)
+dal.init_db(conf, privileges={"read": True, "write": True, "validate": True})
--- test/database/test_dal.py
+++ test/database/test_dal.py
@@ -3,8 +3,11 @@ import pytest
 from gresq.database.dal import dal
 from gresq.database import Base
 from gresq.config import Config
-from gresq.util.csv2db3 import build_db
+#from gresq.util.csv2db3 import build_db
+from gresq.database.models import Sample
+from faker import Faker
 
+fake = Faker()
 config_prefix = 'TEST_DATABASE'
 config_suffix = '_ADMIN'
 #csv2db_file = os.environ["CSV2DB_FILE"]
@@ -13,26 +16,29 @@ csv2db_file = os.path.join(os.getcwd(),'data', 'SEM_Raman_Data')
 
 
 @pytest.fixture
-def dal_conn():
+def dal_conn(scope="module"):
     conf = Config(prefix=config_prefix, suffix=config_suffix, debug=True, try_secrets=False)
     dal.init_db(conf, privileges={"read": True, "write": True, "validate": True})
     # Add some data here
     yield
     # Drop and ditch
-    Base.metadata.drop_all(bind=dal.engine)
+    #Base.metadata.drop_all(bind=dal.engine)
     dal.engine.dispose()
 
-@pytest.fixture
-def testdb(scope="module"):
-    conf = Config(prefix=config_prefix, suffix=config_suffix, debug=True, try_secrets=False)
-    dal.init_db(conf, privileges={"read": True, "write": True, "validate": True})
-    # Add some data here
-    with dal.session_scope() as session:
-        build_db(session, os.path.join(os.getcwd(),'data'), csv2db_file, nrun=34, box_config_path=BOX_CONFIG_PATH)
-    yield
-    # Drop and ditch
-    Base.metadata.drop_all(bind=dal.engine)
-    dal.engine.dispose()
+def session(scope="funcion"):
+    return dal.session_scope()
+
+# @pytest.fixture
+# def testdb(scope="module"):
+#     conf = Config(prefix=config_prefix, suffix=config_suffix, debug=True, try_secrets=False)
+#     dal.init_db(conf, privileges={"read": True, "write": True, "validate": True})
+#     # Add some data here
+#     with dal.session_scope() as session:
+#         build_db(session, os.path.join(os.getcwd(),'data'), csv2db_file, nrun=3, box_config_path=BOX_CONFIG_PATH)
+#     yield
+#     # Drop and ditch
+#     Base.metadata.drop_all(bind=dal.engine)
+#     dal.engine.dispose()
     
 
 class TestDataAccessLayer:
@@ -40,16 +46,23 @@ class TestDataAccessLayer:
         """Test dal._init_db using the TEST_DATABASE_URL_ADMIN config. If this is
         not set ing the environment, the default engine will be sqlite.
         """
+        pass
 
-    def test_csv2db3_build_db(self, testdb):
-        return 1
-        #with dal.session_scope() as session:
-        #    build_db(session, os.path.join(os.getcwd(),'data'), csv2db_file, nrun=34, box_config_path=BOX_CONFIG_PATH)
-        #    #Base.metadata.drop_all(bind=dal.engine)
+class TestSample:
+    def test_create(self, dal_conn, session):
+        pass
+
+
+    # def test_csv2db3_build_db(self, testdb):
+    #     return 1
+    #     #with dal.session_scope() as session:
+    #     #    build_db(session, os.path.join(os.getcwd(),'data'), csv2db_file, nrun=34, box_config_path=BOX_CONFIG_PATH)
+    #     #    #Base.metadata.drop_all(bind=dal.engine)
     
-    def test_some_query(self, testdb):
-        with dal.session_scope() as session:
-            print("How's it going?")
+    # def test_some_query(self, testdb):
+    #     with dal.session_scope() as session:
+    #         for row in session.query(sample):
+    #             print(row.id)
 
         
         
