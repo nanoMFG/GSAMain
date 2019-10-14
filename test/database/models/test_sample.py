8b68e8bbb93a19cd10f173f6ecbac16be1ff0173
Darren Adams <dadamsncsa@gmail.com>
2019-10-14 07:09:17 -0500

    Removed old tests, cleaned up fixtures.

--- test/database/models/test_sample.py
+++ test/database/models/test_sample.py
@@ -1,3 +1,15 @@
+""" Tests for the Sample model
+
+Test relationships, properties and delete cascade behavior.
+
+Conventions:
+
+  test naming:
+    test_rel__arelationship: 
+    test_prop__aproperty:
+    
+
+"""
 import pytest
 
 from gresq.config import Config
@@ -8,19 +20,22 @@ from .. import config_prefix, config_suffix
 
 @pytest.fixture(scope="class")
 def sample():
-    #conf = Config(prefix=config_prefix, suffix=config_suffix, debug=True, try_secrets=False)
-    #dal.init_db(conf, privileges={"read": True, "write": True, "validate": True})
-    print("Hey, here come some samples...")
+    """Set up a set of samples for testing using the factory boy factories.
+    The sample factory cascades to all related tables. 
+    """
+    # create_all is already in test/database__init__.py. 
+    #  I don't know why I have to call it here again, but I do.
     Base.metadata.create_all(bind=dal.engine)
     # Add some data here
-
+    print("Hey, here come some samples...")
     yield SampleFactory.create_batch(5)
+    # Drop and ditch
+    print("Tearing down test samples and DB")
     with dal.session_scope(autocommit=True) as sess:
         for s in sess.query(Sample).all():
             sess.delete(s)
-    # Drop and ditch
     Base.metadata.drop_all(bind=dal.engine)
-    #dal.engine.dispose()
+
 @pytest.fixture
 def sesh():
     print("new sesh")
@@ -29,62 +44,71 @@ def sesh():
     #print("remove sesh")
     #s.close()
 
-class TestSample:
-    def test_simple(self, sample, sesh):
-        
-        #print(sample[8].id)
-        #with dal.session_scope() as sess:
-            for row in sesh.query(Sample).all():
-                print(f"row: {row.id}, {row.nanohub_userid}, {row.material_name}, {row.experiment_date}")
+@pytest.fixture(scope="class")
+def query():
+    """Provide a query with all current samples
+    
+    Returns:
+        Query: All samples
+    """
+    sesh =  dal.Session()
+    return sesh.query(Sample).all()
 
-    # def test_delete__1stsample_cascade(self, sample):
-    #     with dal.session_scope(autocommit=True) as sess:
-    #         smpl = sess.query(Sample).filter(Sample.id==1).one()
-    #         assert smpl.id == 1
-    #         sess.delete(smpl)
-    #         print(sess.query(Sample.id).all()[0][0])
-    #     # Check id not in sess
-    #     # check id not in recipe
-    #     #...
+class TestQueries:
+    def test_simple(self, sample, query):
+        for row in query:
+            print(f"row: {row.id}, {row.nanohub_userid}, {row.material_name}, {row.experiment_date}")
 
-    def test_recipe_rel(self, sample, sesh):
-        #sess = dal.Session()
-        for r, s in zip(sesh.query(Sample).all(), sample):
+    def test_rel__recipe(self, sample, query):
+        for r, s in zip(query, sample):
             print(f"{s.recipe.id}, {r.recipe.id}, {s.id}, {r.recipe.sample_id}")
             assert s.recipe.id == r.recipe.id
             assert s.id == r.recipe.sample_id
 
-    def test_properties_rel(self, sample, sesh):
-        q = sesh.query(Sample).all()
-        for r, s in zip(q, sample):
-            print(f"{s.properties.id}, {r.properties.id}, {s.id}, {r.properties.sample_id}")
-
-        assert all(
-                [s.properties.id == r.properties.id for r, s in zip(q, sample)]
-                )
-        assert all(
-                [s.id == r.properties.sample_id for r, s in zip(q, sample)]
-                )
-
-    def test_prop__author_last_names(self, sample, sesh):
-        q = sesh.query(Sample).all()
-        for r, s in zip(q, sample):
-            print(r.author_last_names)
-        assert all (
-            [s.author_last_names == r.author_last_names for r, s in zip(q, sample)]
-        )
-
-    def test__json_encodable(self, sample, sesh):
-        q = sesh.query(Sample).all()
-        for r in q:
-            print(r.json_encodable())
-
-    def test_rel__raman_file(self, sample, sesh):
-        q = sesh.query(Sample).all()
-        for r in q:
+    def test_rel__raman_files(self, sample, query):
+        for r in query:
             for f in r.raman_files:
                 print(f"{f.filename}, {f.url}, {f.wavelength}")
 
+    def test_rel__properties(self, sample, query):
+        for r in query:
+            print(f"{r.properties.id}, {r.properties.sample_id}, \
+            {r.properties.average_thickness_of_growth}, \
+            {r.properties.shape}")
+
+        assert all(
+                [s.properties.id == r.properties.id for r, s in zip(query, sample)]
+                )
+        assert all(
+                [s.id == r.properties.sample_id for r, s in zip(query, sample)]
+                )
+
+    def test_prop__author_last_names(self, sample, query):
+        for r, s in zip(query, sample):
+            print(r.author_last_names)
+        assert all (
+            [s.author_last_names == r.author_last_names for r, s in zip(query, sample)]
+        )
+
+    def test__json_encodable(self, sample, query):
+        for r in query:
+            print(r.json_encodable())
+
+
+
+class TestDelete:
+    def test_delete_a_sample(self, sample):
+        return 1
+        
+    def test_delete__1stsample_cascade(self, sample):
+        with dal.session_scope(autocommit=True) as sess:
+            smpl = sess.query(Sample).filter(Sample.id==1).one()
+            assert smpl.id == 1
+            sess.delete(smpl)
+            print(sess.query(Sample.id).all()[0][0])
+        # Check id not in sess
+        # check id not in recipe
+        #...
 
 
 
