280ac23667bbf48448bdeabff9d3fcb3ffc6556b
Darren Adams <dadamsncsa@gmail.com>
2019-10-14 10:19:21 -0500

    Added maximum temperature test to recipe testing

--- test/database/models/test_recipe.py
+++ test/database/models/test_recipe.py
@@ -17,3 +17,16 @@ Conventions:
 class TestQueries:
     def test_simple(self, sample, all_sample_query):
         pass
+
+    def test_rel__preparation_steps(self, sample, all_sample_query):
+        pass
+
+    def test_prop__maximum_temperature(self, sample, all_sample_query):
+        for r in all_sample_query:
+            print(
+                f"{r.recipe.maximum_temperature}"
+            )
+        assert all(
+            [r.recipe.maximum_temperature == s.recipe.maximum_temperature 
+             for r, s in zip(all_sample_query, sample)]
+        )
